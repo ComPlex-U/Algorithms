889840471aa01bae2032e9d9a7eddbd227ab6b1e
johngstephen <jonhgstephen@gmail.com>
2020-02-25 13:33:36 +0000

    Create Pilha.py

---
+++ guides to algorithms/Guia06/Python/HashTable/Pilha.py
@@ -0,0 +1,46 @@
+from io import StringIO
+
+class Pilha:
+
+    n = 0
+    top = -1
+    S = []    
+
+    def __getitem__(self, index):
+        return self.S[index]
+
+    def __setitem__(self, key, item):
+        self.S[key] = item
+    
+    def __init__(self, n):
+        self.n = n
+        self.top = -1
+        self.S = [0] * n
+
+    def stack_empty(self):
+        if self.top == -1:
+            return True
+        else:
+            return False
+
+    def push(self, item):
+        self.top = self.top + 1
+        self.S[self.top] = item
+
+    def pop(self):
+        if self.stack_empty is False:
+            print("underflow")
+            return 0
+        else:
+            self.top = self.top - 1
+            return self.S[self.top+1]
+
+    def size(self):
+        return len(self.S)
+
+    def toString(self):
+        txt = StringIO()
+        for k in self.S:
+            txt.write("{}".format(k))
+            txt.write(" ")
+        return txt.getvalue()
